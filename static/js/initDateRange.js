6d45ac234dc8d470296d759c09cac5565c29bd44
Peter Gravelin <pygravelin@gmail.com>
2019-08-01 01:34:10 -0400

    Add functioning date ranges, button diable if invalid

--- static/js/initDateRange.js
+++ static/js/initDateRange.js
@@ -1,3 +1,39 @@
 $('.input-daterange input').each(function() {
     $(this).datepicker('clearDates');
+    console.log("K");
 });
+
+$('.datepicker').each(function (){
+    $(this).datepicker({
+        autoclose: true,
+        format: 'dd-mm-yyyy',
+        todayBtn: "linked"
+    });
+    console.log("EEK");
+});
+
+var sDate,eDate;
+$("#start_date").datepicker().on('changeDate',function(e){
+    console.log("Start change")
+    sDate = new Date($(this).datepicker('getUTCDate'));
+    checkDate();
+});
+
+$("#end_date").datepicker().on('changeDate',function(date){
+    console.log("end change")
+    eDate = new Date($(this).datepicker('getUTCDate'));
+    checkDate();
+});
+
+function checkDate()
+{
+    if(sDate && eDate && (eDate<sDate))
+    {
+        
+        $("#btn_submit").attr("disabled", true);
+    }
+    else
+    {
+        $("#btn_submit").attr("disabled", false);
+    }
+}
